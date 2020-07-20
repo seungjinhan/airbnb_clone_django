6f3b62d963a8298530e580ae2310363743b7edd8
hansj <hanblues@gmail.com>
2020-07-21 00:10:25 +0900

    20200721

---
+++ rooms/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ AbstractItem """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType """
+
+    class Meta:
+        verbose_name_plural = "Room Types"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule """
+
+    class Meta:
+        _name_plural = "House Rules"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo model """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room model """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
+
+    def __str__(self):
+        return self.name
+
