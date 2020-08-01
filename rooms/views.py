8dfa66e3ec945f48347ec4f245347dc3bedc4eb9
hansj <hanblues@gmail.com>
2020-08-01 22:44:06 +0900

    'not_auto_page'

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+import math
 from datetime import datetime
 from django.shortcuts import render
 from . import models
@@ -5,7 +6,13 @@ from . import models
 
 def all_rooms(request):
 
-    all_rooms = models.Room.objects.all()
+    page = request.GET.get('page', 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = math.ceil(models.Room.objects.count() / page_size)
 
     return render(request, 'rooms/all_rooms.html', context={
-        'rooms': all_rooms})
+        'rooms': all_rooms, 'page': page, 'page_count': page_count, 'page_range': range(0, page_count)})
