85cf00595a32c2c44c4d11aa73d1faaeda08371e
hansj <hanblues@gmail.com>
2020-08-08 16:25:38 +0900

    'form'

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,12 @@
 # from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
 from django.utils import timezone
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.http import Http404
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from django_countries import countries
-from . import models
+from django.core.paginator import Paginator
+from . import models, forms
 
 
 class HomeView(ListView):
@@ -33,104 +33,97 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-def search(request):
-    city = request.GET.get('city', 'Anywhere')
-    city = str.capitalize(city)
-    country = request.GET.get('country', 'KR')
-    room_type = int(request.GET.get('room_type', 0))
-    price = request.GET.get('price', 0)
-    guests = request.GET.get('guests', 0)
-    bedrooms = request.GET.get('bedrooms', 0)
-    beds = request.GET.get('beds', 0)
-    baths = request.GET.get('baths', 0)
-    instant = bool(request.GET.get('instant', False))
-    superhost = bool(request.GET.get('superhost', False))
-    s_amenities = request.GET.getlist('amenities')
-    s_facilities = request.GET.getlist('facilities')
+class SearchView(View):
 
-    print(s_facilities)
-    form = {
-        'city': city,
-        's_country': country,
-        's_room_type': room_type,
-        'price': price,
-        'guests': guests,
-        'bedrooms': bedrooms,
-        'beds': beds,
-        'baths': baths,
-        's_amenities': s_amenities,
-        's_facilities': s_facilities,
-        'instant': instant,
-        'superhost': superhost,
-    }
+    def get(self, request):
 
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
+        country = request.GET.get('country')
 
-    choices = {
-        'countries': countries,
-        'room_types': room_types,
-        'amenities': amenities,
-        'facilities': facilities
-    }
+        if country:
+            form = forms.SearchForm(request.GET)
+            if form.is_valid():
 
-    filter_args = {}
+                city = form.cleaned_data.get('city')
+                country = form.cleaned_data.get('country')
+                room_type = form.cleaned_data.get('room_type')
+                price = form.cleaned_data.get('price')
+                guests = form.cleaned_data.get('guests')
+                bedrooms = form.cleaned_data.get('bedrooms')
+                beds = form.cleaned_data.get('beds')
+                baths = form.cleaned_data.get('baths')
+                instant_book = form.cleaned_data.get('instant_book')
+                superhost = form.cleaned_data.get('superhost')
+                amenities = form.cleaned_data.get('amenities')
+                facilities = form.cleaned_data.get('facilites')
 
-    if city != 'Anywhere':
-        filter_args['city__startswith'] = city
+                filter_args = {}
 
-    filter_args["country"] = country
+                if city != 'Anywhere':
+                    filter_args['city__startswith'] = city
 
-    if room_type != 0:
-        filter_args['room_type__pk'] = room_type
+                filter_args["country"] = country
 
-    if price != 0:
-        filter_args['price__lte'] = price
+                if room_type is not None:
+                    filter_args['room_type'] = room_type
 
-    if guests != 0:
-        filter_args['guest__gte'] = guests
+                if price is not None:
+                    filter_args['price__lte'] = price
 
-    if bedrooms != 0:
-        filter_args['bedrooms__gte'] = bedrooms
+                if guests is not None:
+                    filter_args['guest__gte'] = guests
 
-    if beds != 0:
-        filter_args['beds__gte'] = beds
+                if bedrooms is not None:
+                    filter_args['bedrooms__gte'] = bedrooms
 
-    if baths != 0:
-        filter_args['baths__gte'] = baths
+                if beds is not None:
+                    filter_args['beds__gte'] = beds
 
-    if instant is True:
-        filter_args['instant_book'] = True
+                if baths is not None:
+                    filter_args['baths__gte'] = baths
 
-    if superhost is True:
-        filter_args['host__superhost'] = True
+                if instant_book is True:
+                    filter_args['instant_book'] = True
 
-    if len(s_amenities) > 0:
-        for a in s_amenities:
-            filter_args['amenities__pk'] = int(a)
+                if superhost is True:
+                    filter_args['host__superhost'] = True
 
-    if len(s_facilities) > 0:
-        for a in s_facilities:
-            filter_args['facilities__pk'] = int(a)
+                for a in amenities:
+                    filter_args['amenities'] = a
 
-    rooms = models.Room.objects.filter(**filter_args)
+                for a in facilities:
+                    filter_args['facilities'] = a
 
-    return render(
-        request,
-        'rooms/search.html',
-        {
-            **form,
-            **choices,
-            "rooms": rooms
-        })
+                qs = models.Room.objects.filter(
+                    **filter_args).order_by('-created')
+
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get('page', 1)
+
+                rooms = paginator.get_page(page)
+
+            return render(
+                request,
+                'rooms/search.html',
+                {
+                    "form": form, "rooms": rooms
+                })
+        else:
+            form = forms.SearchForm()
+
+        return render(
+            request,
+            'rooms/search.html',
+            {
+                "form": form
+            })
 
 # def room_detail(request, pk):
 
 #     try:
 #         room = models.Room.objects.get(pk=pk)
 #         return render(request, 'rooms/detail.html', {'room': room})
-#     except models.Room.DoesNotExist:
+#     except models.Room.DoesnotExist:
 #         # return redirect(reverse('core:home'))
 #         raise Http404()
 
