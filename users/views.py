c799a874b9b5b4ee19e178a56b239f5e0e29c9d6
hansj <hanblues@gmail.com>
2020-08-08 22:12:18 +0900

    'authentication'

--- users/views.py
+++ users/views.py
@@ -1,29 +1,63 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
+class LoginView(FormView):
 
-    def get(self, req):
-        form = forms.LoginForm(initial={'email': 'hanblues@gmail.com'})
-        return render(req, 'users/login.html', {'form': form})
+    template_name = 'users/login.html'
+    form_class = forms.LoginForm
+    success_url = reverse_lazy('core:home')
 
-    def post(self, req):
-        form = forms.LoginForm(req.POST)
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(req, username=email, password=password)
-            if user is not None:
-                login(req, user)
-                return redirect(reverse('core:home'))
+    # def post(self, req):
+    #     form = forms.LoginForm(req.POST)
 
-        return render(req, 'users/login.html', {'form': form})
+    #     if form.is_valid():
+    #         email = form.cleaned_data.get('email')
+    #         password = form.cleaned_data.get('password')
+    #         user = authenticate(req, username=email, password=password)
+    #         if user is not None:
+    #             login(req, user)
+    #             return redirect(reverse('core:home'))
+
+    #     return render(req, 'users/login.html', {'form': form})
 
 
 def log_out(req):
     logout(req)
     return redirect(reverse('core:home'))
+
+# class LoginView(View):
+
+#     def get(self, req):
+#         form = forms.LoginForm(initial={'email': 'hanblues@gmail.com'})
+#         return render(req, 'users/login.html', {'form': form})
+
+#     def post(self, req):
+#         form = forms.LoginForm(req.POST)
+
+#         if form.is_valid():
+#             email = form.cleaned_data.get('email')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(req, username=email, password=password)
+#             if user is not None:
+#                 login(req, user)
+#                 return redirect(reverse('core:home'))
+
+#         return render(req, 'users/login.html', {'form': form})
+
+
+# def log_out(req):
+#     logout(req)
+#     return redirect(reverse('core:home'))
