7bb904fe5fcce071e5a93a99e6e1cf36da6279a0
hansj <hanblues@gmail.com>
2020-08-08 21:36:23 +0900

    'login-out'

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, req):
+        form = forms.LoginForm(initial={'email': 'hanblues@gmail.com'})
+        return render(req, 'users/login.html', {'form': form})
+
+    def post(self, req):
+        form = forms.LoginForm(req.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(req, username=email, password=password)
+            if user is not None:
+                login(req, user)
+                return redirect(reverse('core:home'))
+
+        return render(req, 'users/login.html', {'form': form})
+
+
+def log_out(req):
+    logout(req)
+    return redirect(reverse('core:home'))
